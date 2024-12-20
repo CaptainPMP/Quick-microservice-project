9eb466092c18921d30e7860ebb805b34694eeeef
CaptainPMP <scikung8@gmail.com>
2024-12-20 12:17:02 +0700

    delete some unused code

--- notification-service/server.js
+++ notification-service/server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const notificationRoutes = require('./routes/notificationRoutes');
-// const cors = require('cors');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-// app.use(cors());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
